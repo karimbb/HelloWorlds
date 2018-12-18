7fd4598647adb4056d251254d2fcd04dfa36c9be
karim <karim_eco@hotmail.com>
2018-12-18 11:43:48 +0100

    C1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Hello
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(createMessage());
+        }
+        public static string createMessage()
+        {
+            return "Hello worlds";
+        }
+    }
+}
